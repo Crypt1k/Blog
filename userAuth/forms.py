30378947483316e442248116019eb44aef7e2da7
vzhidkov <cryptic.kh.ua@gmail.com>
2014-11-17 22:33:49 +0200

    register modelForm

--- userAuth/forms.py
+++ userAuth/forms.py
@@ -1,1 +1,22 @@
+from django.utils.translation import gettext as _
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from django import forms
+
+class RegisterForm(ModelForm):
+    password1 = forms.CharField(label="Password",
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password (again)",
+                                widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(_('The two password fields didn\'t match'))
+        return password2
--- userAuth/views.py
+++ userAuth/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from userAuth.forms import RegisterForm
 
 
 def register_view(request):
-    return render(request, 'userAuth/register.html')
+    #user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword', first_name='fn', last_name='ln')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/myapp')
+    else:
+        form = RegisterForm()
+    return render(request, 'userAuth/register.html', {'form': form})
