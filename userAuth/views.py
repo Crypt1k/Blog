1323bc4560cc6d1f54bfdb4b8727cab9896b320a
vzhidkov <cryptic.kh.ua@gmail.com>
2014-11-20 18:29:25 +0200

    login after registration

--- userAuth/views.py
+++ userAuth/views.py
@@ -1,12 +1,16 @@
-from django.views.generic.edit import FormView
+from django.views import generic
 from userAuth.forms import RegisterForm
+from django.contrib.auth import authenticate, login
 
 
-class RegisterView(FormView):
+class RegisterView(generic.CreateView):
     template_name = 'userAuth/register.html'
     form_class = RegisterForm
     success_url = '/'
 
     def form_valid(self, form):
+        new_user = authenticate(username=self.request.POST['username'],
+                                password=self.request.POST['password'])
+        login(self.request, new_user)
         form.save()
         return super(RegisterView, self).form_valid(form)
