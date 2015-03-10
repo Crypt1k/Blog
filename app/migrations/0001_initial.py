3c3361377fde435a6a53de8c50b92142961dee97
Crypt1k <cryptic.kh.ua@gmail.com>
2015-03-10 16:18:24 +0200

    migration fix

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -15,16 +15,37 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Article',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('headline', models.CharField(max_length=100)),
                 ('slug', models.SlugField(unique=True, max_length=100, editable=False)),
                 ('content', models.TextField()),
                 ('pub_date', models.DateField()),
-                ('reporter', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'ordering': ('-pub_date',),
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Label',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('name', models.CharField(unique=True, max_length=100)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='labels',
+            field=models.ManyToManyField(to='app.Label'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='reporter',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
         ),
     ]
