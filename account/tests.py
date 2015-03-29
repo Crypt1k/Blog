24f9748ab1ebbd8eff80ea1c9a7ad1b4c0378d4a
vzhidkov <cryptic.kh.ua@gmail.com>
2015-03-29 20:00:26 +0300

    added tests

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from account.forms import RegisterForm
 
-# Create your tests here.
+
+class AccountTests(TestCase):
+    def test_blank_register_form(self):
+        """
+        If form empty, an appropriate message should be displayed.
+        """
+        form = RegisterForm({})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'username': ['This field is required.'],
+            'password1': ['This field is required.'],
+            'password2': ['This field is required.']
+            })
+
+    def test_create_user(self):
+        """ User creation test """
+        form = RegisterForm({'username': 'Admin',
+                             'first_name': 'Name',
+                             'last_name': 'LastName',
+                             'email': 'admin@gmail.com',
+                             'password1': '12345',
+                             'password2': '12345'})
+        self.assertTrue(form.is_valid())
+        user = form.save()
+        self.assertEqual(user.username, 'Admin')
+        self.assertEqual(user.first_name, 'Name')
+        self.assertEqual(user.last_name, 'LastName')
+        self.assertEqual(user.email, 'admin@gmail.com')
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,113 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+from app.models import Article, Label
+
+
+class NoArticleViewTests(TestCase):
+    def test_list_view_with_no_articles(self):
+        """
+        If no articles exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('app:article_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No articles found.')
+        self.assertQuerysetEqual(response.context['articles'], [])
+
+
+class ArticleViewTests(TestCase):
+    def setUp(self):
+        """ Need at least one article """
+        user = User.objects.create_user(username='admin',
+                                        email='admin@gmail.com',
+                                        password='top_secret')
+
+        label = Label.objects.create(name='Label1')
+
+        article = Article.objects.create(headline='First Topic',
+                                         content='Content for 1st topic',
+                                         pub_date=timezone.now(),
+                                         reporter=user)
+        article.labels.add(label.id)
+
+    def test_article_list_view(self):
+        date = timezone.now()
+        response = self.client.get(reverse('app:article_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'First Topic')
+        response = self.client.get(reverse('app:article_detail',
+                                           kwargs=dict(
+                                               year=date.year,
+                                               month=date.month,
+                                               day=date.day,
+                                               slug='first-topic'
+                                           )))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'First Topic')
+
+    def test_article_list_fail_view(self):
+        """ If no articles exist """
+        date = timezone.now()
+        response = self.client.get(reverse('app:article_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'First Topic')
+        response = self.client.get(reverse('app:article_detail',
+                                           kwargs=dict(
+                                               year=date.year,
+                                               month=date.month,
+                                               day=date.day,
+                                               slug='fake-topic'
+                                           )))
+        self.assertEqual(response.status_code, 404)
+
+    def test_article_search_view(self):
+        """ Search existing headline """
+        response = self.client.get('%s?search=%s'
+                                   % (reverse('app:article_list')
+                                      , 'First Topic'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'First Topic')
+
+        response = self.client.get('%s?search=%s'
+                                   % (reverse('app:article_list')
+                                      , '1st'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '1st')
+
+        """ Search existing content """
+        response = self.client.get('%s?search=%s'
+                                   % (reverse('app:article_list')
+                                      , 'Content for 1st'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Content for 1st')
+
+        response = self.client.get('%s?search=%s'
+                                   % (reverse('app:article_list')
+                                      , '1st'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '1st')
+
+    def test_article_search_fail_view(self):
+        """ Search fake headline or content """
+        response = self.client.get('%s?search=%s'
+                                   % (reverse('app:article_list')
+                                      , 'Fake data'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No articles found.')
+
+    def test_article_label_view(self):
+        """ List of existing ar """
+        response = self.client.get(reverse('app:article_label',
+                                           kwargs=dict(name='Label1')))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Label1')
+        print(response.content)
+
+    def test_article_label_fail_view(self):
+        """ Test for fake label """
+        response = self.client.get(reverse('app:article_label',
+                                           kwargs=dict(name='Fake')))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No articles found.')
